0d5d679434da121449e9d3d26a4a248cf0263001
zahraaayii <48953412+zahraaayii@users.noreply.github.com>
2019-04-04 20:00:46 +0430

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,15 +3,41 @@
  */
 $(document).ready(function() {
 
+    $('#filter').click(function(e){
+        e.preventDefault();
+    });
+
+    $(".down").click(function(){
+       $('.downloadnone').show()
+        $('.downloadnone').hide(3000)
+    });
+    $(".down-you").click(function(){
+        $('.downloadnone-you').show()
+        $('.downloadnone-you').hide(3000)
+    });
+
+    $('#selall').hide();
     $('li').hover(function () {
             $(this).css('backgroundColor', '#0056b3')
 
         },
         function () {
-            if($(this).hasClass('active1')===true){}
+            if($(this).hasClass('activenavitem')===true){}
             else{
             $(this).css('backgroundColor', '#007bff ')
             }
         }
     );
+    $('#filter').click(function(){
+        if($('select option:selected').val()==='all'){
+            $('#selmyself').hide()
+            $('#selall').show()
+        }
+        else{
+            $('#selmyself').show()
+            $('#selall').hide()
+        }
+    })
+
+
 });

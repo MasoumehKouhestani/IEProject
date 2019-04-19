2b589e90b6fb12a4b4cba687a1c2a4085d892986
zahraaayii <48953412+zahraaayii@users.noreply.github.com>
2019-04-19 14:24:04 +0430

    Update forNinejson.js

--- src/js/forNinejson.js
+++ src/js/forNinejson.js
@@ -23,84 +23,92 @@ $(document).ready(function(){
         '</span><br>\n' +
         '        <br>\n' +
         '        <form method="post">\n' +
-        '
-        '<button type="submit" class="btn btn-success">تایید</button>
-               <button type="submit" class="btn btn-secondary">غیرفعال</button>
-               <input type="button" value="ویرایش پروفایل" class="btn btn-info" data-toggle="modal" data-target="#+
-               m
-              + ">
-               <button type="submit" class="btn btn-danger">حذف</button>'
-
-
-        '        </form>\n' +
-        '      </div>');
+          '<button type="submit" class="btn btn-success">تایید</button>\n' +
+          '      <button type="submit" class="btn btn-secondary">غیرفعال</button>\n' +
+          '      <input type="button" value="ویرایش پروفایل" class="btn btn-info" data-toggle="modal" data-target="#' +
+          m +
+          '">\n' +
+          '      <button type="submit" class="btn btn-danger">حذف</button>\n' +
+          '      </form>\n' +
+          '      </div>\n' +
+          '    </div>\n' +
+          '  </div>\n' +
+          '</div>'
+       );
 
       $('.person_info_container').append(s);
-      $('div class="modal" id="">
-           <div class="modal-dialog modal-dialog-centered modal-lg ">
-             <div class="modal-content">
-               <div class="modal-header">
-                 <h5 class="modal-title">
-                   ویرایش پروفایل کاربر انتخابی
-                 </h5>
-               </div>
-
-               <div class="modal-body">
-                 <div class="row bg-white">
-                   <div class="col-sm">
-                     <div class="boxinmodal text-sm-right ">
-                       <form name="profile_edit_form" onsubmit="return validate_profile_edit()" method="post" class="form-group">
-                         <div class="form-group">
-                           <label for="name_edit_profile">نام و نام خانوادگی:</label>
-                           <input type="text" id="name_edit_profile" name="name_edit_profile" class="form-control"
-                                  placeholder="نام و نام خانوادگی "/>
-
-                           <br>
-                         </div>
-                         <div class="form-group">
-                           <label for="username_edit_profile">نام کاربری:</label>
-                           <input type="text" id="username_edit_profile" name="username_edit_profile" class="form-control"
-                                  placeholder="نام کاربری "/>
-
-                           <br>
-                         </div>
-                         <div class="form-group">
-                           <label for="email_edit_profile">پست الکترونیکی:</label>
-                           <input type="email" id="email_edit_profile" name="email_edit_profile" class="form-control"
-                                  placeholder="پست الکترونیکی "/>
-
-                           <br>
-                         </div>
-                         <div class="form-group">
-                           <label for="new_password_edit_profile">رمز عبور جدید:</label>
-                           <input type="password" id="new_password_edit_profile" name="new_password_edit_profile" class="form-control"
-                                  placeholder="رمز عبور جدید "/>
-
-                         </div>
-                         <div class="form-group">
-                           <label for="repeat_password_edit_profile">تکرار رمز عبور جدید:</label>
-                           <input type="password" id="repeat_password_edit_profile" name="repeat_password_edit_profile"
-                                  class="form-control"
-                                  placeholder="تکرار رمز عبور جدید "/>
-
-                         </div>
-                         <div id="edit_profile_btn_div" class="form-group">
-                           <input id="edit_profile_btn" type="submit" class="form-control btn submit-btn btn-primary" value="اعمال تغییرات"/>
-                         </div>
-                       </form>
-
-               </div>
-             </div>
-                 </div>
-             <div class="modal-footer">
-               <button type="button" class="btn btn-danger" data-dismiss="modal">
-                 بستن
-               </button>
-             </div>
-           </div>
-         </div>
-         </div>')
+      var l=$('<div class="modal" id="' +
+          m+
+          '">\n' +
+          '  <div class="modal-dialog modal-dialog-centered modal-lg ">\n' +
+          '    <div class="modal-content">\n' +
+          '      <div class="modal-header">\n' +
+          '        <h5 class="modal-title">\n' +
+          '          ویرایش پروفایل کاربر انتخابی\n' +
+          '        </h5>\n' +
+          '      </div>\n' +
+          '\n' +
+          '      <div class="modal-body">\n' +
+          '        <div class="row bg-white">\n' +
+          '          <div class="col-sm">\n' +
+          '            <div class="boxinmodal text-sm-right font-weight-bold">\n' +
+          '              <form name="profile_edit_form" onsubmit="return validate_profile_edit()" method="post" class="form-group">\n' +
+          '                <div class="form-group">\n' +
+          '                  <label for="name_edit_profile">نام و نام خانوادگی:</label>\n' +
+          '                  <input type="text" id="name_edit_profile" name="name_edit_profile" class="form-control"\n' +
+          '                         placeholder="نام و نام خانوادگی "/>\n' +
+          '\n' +
+          '                  <br>\n' +
+          '                </div>\n' +
+          '                <div class="form-group">\n' +
+          '                  <label for="username_edit_profile">نام کاربری:</label>\n' +
+          '                  <input type="text" id="username_edit_profile" name="username_edit_profile" class="form-control"\n' +
+          '                         placeholder="نام کاربری "/>\n' +
+          '\n' +
+          '                  <br>\n' +
+          '                </div>\n' +
+          '                <div class="form-group">\n' +
+          '                  <label for="email_edit_profile">پست الکترونیکی:</label>\n' +
+          '                  <input type="email" id="email_edit_profile" name="email_edit_profile" class="form-control"\n' +
+          '                         placeholder="پست الکترونیکی "/>\n' +
+          '\n' +
+          '                  <br>\n' +
+          '                </div>\n' +
+          '                <div class="form-group">\n' +
+          '                  <label for="new_password_edit_profile">رمز عبور جدید:</label>\n' +
+          '                  <input type="password" id="new_password_edit_profile" name="new_password_edit_profile" class="form-control"\n' +
+          '                         placeholder="رمز عبور جدید "/>\n' +
+          '\n' +
+          '                </div>\n' +
+          '                <div class="form-group">\n' +
+          '                  <label for="repeat_password_edit_profile">تکرار رمز عبور جدید:</label>\n' +
+          '                  <input type="password" id="repeat_password_edit_profile" name="repeat_password_edit_profile"\n' +
+          '                         class="form-control"\n' +
+          '                         placeholder="تکرار رمز عبور جدید "/>\n' +
+          '\n' +
+          '                </div>\n' +
+          '                <div id="edit_profile_btn_div" class="form-group">\n' +
+          '                  <input id="edit_profile_btn" type="submit" class="form-control btn submit-btn btn-primary" value="اعمال تغییرات"/>\n' +
+          '                </div>\n' +
+          '              </form>\n' +
+          '\n' +
+          '      </div>\n' +
+          '    </div>\n' +
+          '        </div>\n' +
+          '    <div class="modal-footer">\n' +
+          '      <button type="button" class="btn btn-danger" data-dismiss="modal">\n' +
+          '        بستن\n' +
+          '      </button>\n' +
+          '    </div>\n' +
+          '  </div>\n' +
+          '</div>\n' +
+          '</div>\n' +
+          '\n' +
+          '\n' +
+          '</div>')
+        $('body').append(l);
     }
+
 
   });
 });
